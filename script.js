513b5a446eb702457fb860783f481832750aa559
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-25 19:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //your JS code here. If required.
-  window.addEventListener("resize",function(){
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
     let height=this.innerHeight;
     let width=this.innerWidth;
    let div=document.getElementById("sizeInfo");
